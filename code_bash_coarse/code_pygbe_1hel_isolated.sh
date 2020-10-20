206862a8d13b540ff72ef23516077c47e2e71357
UrzuaSergio <sergio.urzua.6771@gmail.com>
2020-10-20 16:37:19 -0300

    Repro Packs 1hel-sensor with Electric Field

---
+++ code_bash_coarse/code_pygbe_1hel_isolated.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -p gpuk
+#SBATCH --gres=gpu:1
+#SBATCH -J 1hel_isolated_EF1e-12
+#SBATCH -o 1hel_isolated_EF1e-12-%j.out
+#SBATCH -e 1hel_isolated_EF1e-12-%j.err
+#SBATCH --mem=2G
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=sergio.urzua.13@sansano.usm.cl
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+use cuda10
+use anaconda3
+
+export PYTHONPATH=/user/s/surzua/PyCuda/pycuda/lib/python
+
+cd ../../Lysozyme_Sensor_PyGBe_EF1e-12/
+
+/user/s/surzua/PyCuda/pycuda/bin/pygbe 1hel_isolated/
